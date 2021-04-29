0f38fbd99f78f3b31a2872b51e6c40fe23eed885
Tyler Tallman <dspiteself@gmail.com>
2021-04-29 12:07:31 -0500

    update dependiencies for newer datomic

--- project.clj
+++ project.clj
@@ -3,12 +3,13 @@
   :url "http://github.com/appcanary/datomic-statsd-reporter"
   :license {:name "Apache License Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[com.google.cloud/google-cloud-monitoring "1.90.0"]]
+  :dependencies [[com.google.cloud/google-cloud-monitoring "2.2.1" :exclusions [com.google.guava/guava]]
+                 [com.google.cloud/google-cloud-core "1.94.8" :exclusions [com.google.guava/guava]]]
   :profiles
   {:provided
    {:dependencies
     [[org.clojure/clojure "1.10.1"]
-
+     [com.google.guava/guava "30.1-jre"]
      #_[javax.xml.bind/jaxb-api "2.3.1"]
      #_[com.sun.xml.bind/jaxb-core "2.3.0"]
      #_[com.sun.xml.bind/jaxb-impl "2.3.1"]
